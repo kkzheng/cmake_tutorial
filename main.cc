a7b0315397d7c5ce23b6d1e9a5b7f911f648e201
zhangjinhao05 <zhangjinhao05@meituan.com>
2023-01-16 22:15:26 +0800

    add config

--- main.cc
+++ main.cc
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "my_power.h"
+#include "config.h"
+
+#ifdef USE_MYMATH
+  #include "my_power.h"
+#else
+  #include <math.h>
+#endif
 
 int main(int argc, char *argv[]) {
   if (argc < 3) {
@@ -9,7 +15,13 @@ int main(int argc, char *argv[]) {
   }
   double base = atof(argv[1]);
   int exp = atoi(argv[2]);
+#ifdef USE_MYMATH
+  printf("My provided power\n");
   double res = power(base, exp);
+#else
+  printf("Lib provided pow\n");
+  double res = pow(base, exp);
+#endif
   printf("%g ^ %d is %g\n", base, exp, res);
   return 0;
 }
